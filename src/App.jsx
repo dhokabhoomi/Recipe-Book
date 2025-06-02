4ac0d87d40ba5304ef75ca58eb3a1a04403bc111
dhokabhoomi <bhoomidhoka@gmail.com>
2025-06-02 13:35:05 +0530

    set up routing and layout structure for RecipeRipple

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import AddRecipe from "./components/AddRecipe";
+import Search from "./components/Search";
 
-function App() {
-  return <></>;
+export default function App() {
+  return (
+    <Router>
+      <header className="header">
+        <Navbar />
+      </header>
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/AddRecipe" element={<AddRecipe />} />
+          <Route path="/Search" element={<Search />} />
+        </Routes>
+      </main>
+
+      <footer className="footer">
+        <p>© 2025 RecipeRipple. All rights reserved.</p>
+      </footer>
+    </Router>
+  );
 }
-
-export default App;
