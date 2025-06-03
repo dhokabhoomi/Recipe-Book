3c439304afb748c29cadf36bc12afac648367041
dhokabhoomi <bhoomidhoka@gmail.com>
2025-06-03 19:10:14 +0530

    Add state-based refresh to Home on recipe addition

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import "./styles/global.css";
+// import "./styles/global.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import AddRecipe from "./components/AddRecipe";
