af897fbc8b7b35d8ef2570e21ebf12a500e3bd43
dhokabhoomi <bhoomidhoka@gmail.com>
2025-06-03 19:03:13 +0530

    Add state-based refresh to Home on recipe addition

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import "./styles/global.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import AddRecipe from "./components/AddRecipe";
 import Search from "./components/Search";
 
 export default function App() {
+  const [refreshTrigger, setRefreshTrigger] = useState(0);
+
+  const handleRecipeAdded = () => {
+    setRefreshTrigger((prev) => prev + 1);
+  };
+
   return (
     <Router>
-      <header className="header">
-        <Navbar />
-      </header>
-      <main className="main-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/AddRecipe" element={<AddRecipe />} />
-          <Route path="/Search" element={<Search />} />
-        </Routes>
-      </main>
+      <div className="app">
+        <header className="header">
+          <Navbar />
+        </header>
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home key={refreshTrigger} />} />
+            <Route
+              path="/AddRecipe"
+              element={<AddRecipe onRecipeAdded={handleRecipeAdded} />}
+            />
+            <Route path="/Search" element={<Search />} />
+          </Routes>
+        </main>
 
-      <footer className="footer">
-        <p>© 2025 RecipeRipple. All rights reserved.</p>
-      </footer>
+        <footer className="footer">
+          <p>© 2025 RecipeRipple. All rights reserved.</p>
+        </footer>
+      </div>
     </Router>
   );
 }
